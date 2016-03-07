982fa74e6750c19eca99f5ef906688abd600f2e6
James Stanier <jamess@brandwatch.com>
2016-03-07 10:56:47 +0000

    Skeleton Spring Boot application.

--- src/main/java/org/jstanier/probabilistic/Main.java
+++ src/main/java/org/jstanier/probabilistic/Main.java
@@ -1,8 +1,17 @@
 package org.jstanier.probabilistic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        SpringApplication.run(Main.class);
+        logger.info("Hello world");
     }
 }
